5b81897e868fbe495fb4f9a62445ee6d7e9ec8e7
flaymax <69513102+flaymax@users.noreply.github.com>
2020-09-12 22:21:48 +0300

    commit1 test br

--- test123.py
+++ test123.py
@@ -1,2 +1,8 @@
-print('nkrjfn5')
-x=-1
+class sumi:
+    def factorial(self,n):
+        return n * factorial(n - 1) if n > 1 else 1
+    def sum_factorial(self,n):
+        summ=0
+        for i in range(n+1):
+            summ+=factorial(i)
+        return summ
